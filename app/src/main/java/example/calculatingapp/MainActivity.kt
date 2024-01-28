0b57b254c39a3852d118ecff6391127b657e499b
Eddie-Pak <foalstn@naver.com>
2024-01-28 16:44:23 +0900

    minus button & UI update

--- app/src/main/java/example/calculatingapp/MainActivity.kt
+++ app/src/main/java/example/calculatingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package example.calculatingapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,12 +9,14 @@ import android.widget.TextView
 
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val numberTextView = findViewById<TextView>(R.id.numberTextView)
         val resetButton = findViewById<Button>(R.id.resetButton)
+        val minusButton = findViewById<Button>(R.id.minusButton)
         val plusButton = findViewById<Button>(R.id.plusButton)
 
         var number = 0
@@ -25,6 +28,11 @@ class MainActivity : AppCompatActivity() {
 
         plusButton.setOnClickListener {
             number += 1
+            numberTextView.text = number.toString()
+        }
+
+        minusButton.setOnClickListener {
+            number -= 1
             numberTextView.text = number.toString()
         }
 
