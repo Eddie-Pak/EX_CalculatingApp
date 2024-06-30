f8cba4b328ebd24254f9299cd74363bf1d9c84b8
Eddie-Pak <foalstn@naver.com>
2024-07-01 00:23:01 +0900

    viewbinding, weight, saveinstance update

--- app/src/main/java/example/calculatingapp/MainActivity.kt
+++ app/src/main/java/example/calculatingapp/MainActivity.kt
@@ -3,23 +3,23 @@ package example.calculatingapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
+import example.calculatingapp.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var number = 0
+
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val numberTextView = findViewById<TextView>(R.id.numberTextView)
-        val resetButton = findViewById<Button>(R.id.resetButton)
-        val minusButton = findViewById<Button>(R.id.minusButton)
-        val plusButton = findViewById<Button>(R.id.plusButton)
-
-        var number = 0
+        val numberTextView = binding.numberTextView
+        val resetButton = binding.resetButton
+        val minusButton = binding.minusButton
+        val plusButton = binding.plusButton
 
         resetButton.setOnClickListener {
             number = 0
@@ -39,5 +39,16 @@ class MainActivity : AppCompatActivity() {
             numberTextView.text = number.toString()
         }
 
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("number", number)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        number = savedInstanceState.getInt("number")
+        binding.numberTextView.text = number.toString()
+        super.onRestoreInstanceState(savedInstanceState)
     }
 }
